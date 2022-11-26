fd5258eb137d857ee01c65d9df5990e2e2f6b2b2
krishnakantdev <114336910+krishnakantdev@users.noreply.github.com>
2022-11-26 10:33:17 -0800

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off
+
+python -m oslash_assesment sample_input\input1.txt
+python -m oslash_assesment sample_input\input2.txt
+python -m oslash_assesment sample_input\input3.txt
+python -m oslash_assesment sample_input\input4.txt
---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python3 -m krishna
+ sample_input/input1.txt
+python3 -m krishna
+ sample_input/input2.txt
+python3 -m krishna
+ sample_input/input3.txt
+python3 -m krishna
+ sample_input/input4.txt
