f15b1d30743caed6c42d882f0128dc7c2547a030
krishnakantdev <114336910+krishnakantdev@users.noreply.github.com>
2022-11-26 10:40:15 -0800

    Delete run.bat

--- run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-python -m oslash_assesment sample_input\input1.txt
-python -m oslash_assesment sample_input\input2.txt
-python -m oslash_assesment sample_input\input3.txt
-python -m oslash_assesment sample_input\input4.txt
